bd489f76aea1a44ef8e614ef38b577c19e4ebbc7
Naveen Kumashi <naveen.kumashi@hp.com>
2014-06-02 18:19:17 +0530

    unit test cases +  small change

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.m
@@ -0,0 +1,34 @@
+//
+//  GuildBrowserLogicTests.m
+//  GuildBrowserLogicTests
+//
+//  Created by SauravKabra on 6/2/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GuildBrowserLogicTests : XCTestCase
+
+@end
+
+@implementation GuildBrowserLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
